983c1e20cdfee9e5d0899cc8325e09442eaee828
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-02-10 23:33:22 +0530

    feat(jobs-api): add job CRUD api helpers

---
+++ frontend/src/api/jobs.js
@@ -0,0 +1,36 @@
+import apiInstance from "./axios";
+
+// Create new job
+
+export const createJob = async (jobData) => {
+    const response = await apiInstance.post('/jobs', jobData);
+    return response.data;
+}
+
+// Get all jobs
+
+export const getAllJobs = async (params) => {
+    const response = await apiInstance.get('/jobs',{params});
+    return response.data;
+}
+
+// Get Job by Id
+
+export const getJobById = async (id) => {
+    const response = await apiInstance.get(`/jobs/${id}`);
+    return response.data;
+}
+
+// Update job
+
+export const updateJob = async(id, jobData) => {
+    const response = await apiInstance.put(`/jobs/${id}`, jobData);
+    return response.data;
+}
+
+// Delete job
+
+export const deleteJob = async (id) => {
+    const response = await apiInstance.delete(`/jobs/${id}`);
+    return response.data;
+}
