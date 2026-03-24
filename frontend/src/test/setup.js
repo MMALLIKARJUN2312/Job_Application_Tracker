5b2bfb7982e59ed3d72998d82b75aaadf4b5f0a3
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-03-24 23:09:46 +0530

    test(msw): configure global test setup with server lifecycle

--- frontend/src/test/setup.js
+++ frontend/src/test/setup.js
@@ -1,1 +1,11 @@
-import "@testing-library/jest-dom";
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./mocks/server";
+
+// Start server before all tests
+beforeAll(() => server.listen());
+
+// Reset handlers after each test
+afterEach(() => server.resetHandlers());
+
+// Close server after tests
+afterAll(() => server.close());
